57f4aa152ed13b37575cbf2a16a90761574bbc03
Soumoditya <soumodityapramanik@gmail.com>
2025-08-24 12:56:23 +0530

    Create About.jsx

---
+++ Src/Components/About.jsx
@@ -0,0 +1,20 @@
+export default function About() {
+  return (
+    <section>
+      <h1>Soumoditya Pramanik</h1>
+      <h3>Aspiring Full Stack Developer | Tech Enthusiast</h3>
+      {/* Photo section (add your image here later) */}
+      <div style={{width: 150, height: 150, background: '#eee', borderRadius: '50%', margin: '1em auto'}}>
+        {/* Add profile photo here */}
+      </div>
+      <p>
+        Male | Born: 12/10/2004<br />
+        Location: Rampurhat, Birbhum, West Bengal, India<br />
+        Nationality: Indian
+      </p>
+      <p>
+        Hi! I'm Soumoditya, an aspiring developer studying BCA at Brainware University, passionate about building modern web apps and learning new technology.
+      </p>
+    </section>
+  );
+}
