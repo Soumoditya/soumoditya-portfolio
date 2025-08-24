3a61b399d9718a9ddae27d1ca15271161368c8c0
Soumoditya <soumodityapramanik@gmail.com>
2025-08-24 12:55:14 +0530

    Create App.jsx

---
+++ Src/App.jsx
@@ -0,0 +1,19 @@
+import About from './components/About';
+import Education from './components/Education';
+import Skills from './components/Skills';
+import Projects from './components/Projects';
+import Certificate from './components/Certificate';
+import Contact from './components/Contact';
+
+export default function App() {
+  return (
+    <div>
+      <About />
+      <Education />
+      <Skills />
+      <Projects />
+      <Certificate />
+      <Contact />
+    </div>
+  );
+}
