a6fabdfc7d2dd9d8114a8e9861bfff5e517a36ed
Soumoditya <soumodityapramanik@gmail.com>
2025-08-24 13:05:20 +0530

    Create Contact.jsx

---
+++ Src/Components/Contact.jsx
@@ -0,0 +1,18 @@
+export default function Contact() {
+  return (
+    <section>
+      <h2>Contact</h2>
+      <ul>
+        <li>Email: <a href="mailto:soumodityapramanik@gmail.com">soumodityapramanik@gmail.com</a></li>
+        <li>Phone: <a href="tel:+919064882049">+91 9064882049</a></li>
+        <li>WhatsApp: <a href="https://wa.me/919064882049">+91 9064882049</a></li>
+        <li>LinkedIn: <a href="https://www.linkedin.com/in/soumoditya-pramanik-598754322" target="_blank">View Profile</a></li>
+        <li>GitHub: <a href="https://github.com/Soumoditya" target="_blank">github.com/Soumoditya</a></li>
+        <li>Pincode: 731224</li>
+      </ul>
+      <p>
+        Rampurhat, Birbhum, West Bengal, India
+      </p>
+    </section>
+  );
+}
