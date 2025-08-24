0cf474fa901822e7c4a45c87564cdfe610821cb1
Soumoditya <soumodityapramanik@gmail.com>
2025-08-24 13:04:30 +0530

    Create Certificate.jsx

---
+++ Src/Components/Certificate.jsx
@@ -0,0 +1,14 @@
+export default function Certificate() {
+  return (
+    <section>
+      <h2>Certificates</h2>
+      <ul>
+        <li>
+          <a href="https://www.mygreatlearning.com/certificate/ZHMMOTWK" target="_blank" rel="noopener noreferrer">
+            Great Learning Completion Certificate
+          </a>
+        </li>
+      </ul>
+    </section>
+  );
+}
